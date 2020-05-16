44c9fec6ad517a04eeca7b5d70bfb5525565b2ca
ajlewis02 <ajlewis@wpi.edu>
2020-05-16 13:38:46 -0400

    Groundwork for saving data as csv, including marking interpolated values as such.
    All print statements now depend on verbose

--- Vicon.py
+++ Vicon.py
@@ -64,13 +64,34 @@ class Vicon(object):
         self._force_plates = {}
         self._IMUs = {}
         self._accels = {}
+
+        #  nan_dict is a dictionary with the same format of data_dict,
+        #  but it keeps track of the positions of nans that get interpolated.
+
+        #  data is accessed through nan_dict[category][subject][field],
+        #  where nan_dict contains category iff there is at least one valid subject for the category,
+        #  nan_dict[category] contains subject iff there is at least one valid field for the subject, and
+        #  nan_dict[category][subject] contains field iff data_dict[category][subject][field]["data"] exists.
+
+        #  nan_dict[category][subject][field] is a boolean array where
+        #  nan_dict[category][subject][field] = np.isnan(data_dict[category][subject][field]["data"])
+        #  or, nan_dict[category][subject][field][n] = np.isnan(data_dict[category][subject][field]["data"][n])
+        #  iff data_dict[category][subject][field]["data"] contained missing values that were interpolated.
+
+        #  if data_dict[category][subject][field]["data"] did not contain any missing values,
+        #  or if data_dict[category][subject][field]["data"] consisted solely of nans,
+        #  nan_dict[category][subject][field] is an array consisting only of False,
+        #  where len(nan_dict[category][subject][field]) = len(data_dict[category][subject][field]["data"])
+
+        self._nan_dict = {}
+
         self.data_dict = self.open_vicon_file(self._file_path, verbose)
-        self._make_Accelerometers()
-        self._make_EMGs()
-        self._make_force_plates()
-        self._make_IMUs()
+        self._make_Accelerometers(verbose=verbose)
+        self._make_EMGs(verbose=verbose)
+        self._make_force_plates(verbose=verbose)
+        self._make_IMUs(verbose=verbose)
         self._make_marker_trajs()
-        self._make_model()
+        self._make_model(verbose=verbose)
 
     def _find_number_of_frames(self, col):
         """
@@ -264,17 +285,18 @@ class Vicon(object):
         return list(filter(lambda x: substring in x, sensors.keys()))
 
 
-    def _make_model(self):
+    def _make_model(self, verbose=False):
         """
         generates a model from the model outputs
         :return:
         """
         if "Model Outputs" in self.data_dict:
             self._model_output = ModelOutput.ModelOutput(self.data_dict["Model Outputs"], self.joint_names)
-        else:
+            print "Model Outputs generated"
+        elif verbose:
             print "No Model outputs"
 
-    def _make_force_plates(self):
+    def _make_force_plates(self, verbose=False):
         """
         generate force plate models
         :return: None
@@ -294,15 +316,16 @@ class Vicon(object):
                                                               sensors["Force_Plate__Force_2"],
                                                               sensors["Force_Plate__Moment_2"],
                                                               sensors["Force_Plate__CoP_2"])
-            else:
+                print "Force plate models generated"
+            elif verbose:
                 print "No force plates"
-        else:
-            print "No Devices"
+        elif verbose:
+            print "A scan for force plates found no Devices"
 
     def _make_markers(self):
         markers = self.data_dict["Trajectories"]
 
-    def _make_EMGs(self):
+    def _make_EMGs(self, verbose=False):
         """
         generate EMG models
         :return: None
@@ -316,12 +339,13 @@ class Vicon(object):
                 for e_key, t_key in zip(EMG_keys, T_EMG_keys):
                     self._T_EMGs[int(filter(str.isdigit, t_key))] = EMG.EMG(t_key, sensors[t_key]["EMG"])
                     self._EMGs[int(filter(str.isdigit, e_key))] = EMG.EMG(e_key, sensors[e_key]["IM EMG"])
-            else:
+                print "EMG models generated"
+            elif verbose:
                 print "No EMGs"
-        else:
-            print "No Devices"
+        elif verbose:
+            print "A scan for EMGs found no Devices"
 
-    def _make_IMUs(self):
+    def _make_IMUs(self, verbose=False):
         """
         generate IMU models
         :return: None
@@ -332,10 +356,11 @@ class Vicon(object):
                 keys = self._filter_dict(sensors, 'IMU')
                 for key in keys:
                     self._IMUs[int(filter(str.isdigit, key))] = IMU.IMU(key, sensors[key])
-            else:
+                print "IMU models Generated"
+            elif verbose:
                 print "No IMUs"
-        else:
-            print "No Devices"
+        elif verbose:
+            print "A scan for IMUs found no Devices"
 
     def _make_marker_trajs(self):
         """
@@ -345,7 +370,7 @@ class Vicon(object):
         self._markers = Markers.Markers(self.data_dict["Trajectories"])
         self._markers.make_markers()
 
-    def _make_Accelerometers(self):
+    def _make_Accelerometers(self, verbose=False):
         """
         generate the accel objects
         :return: None
@@ -356,15 +381,17 @@ class Vicon(object):
                 keys = self._filter_dict(sensors, 'Accel')
                 for key in keys:
                     self._accels[int(filter(str.isdigit, key))] = Accel.Accel(key, sensors[key])
-            else:
+                print "Accel models generated"
+            elif verbose:
                 print "No Accels"
-        else:
-            print "No Devices"
+        elif verbose:
+            print "A scan for Accels found no Devices"
 
     def open_vicon_file(self, file_path, verbose=False):
         """
         parses the Vicon sensor data into a dictionary
         :param file_path: file path
+        :param verbose: prints debug statements if True
         :return: dictionary of the sensors
         :rtype: dict
         """
@@ -378,7 +405,7 @@ class Vicon(object):
         names, segs = self._seperate_csv_sections(raw_data)
 
         for index, output in enumerate(names):
-            data[output] = self._extract_values(raw_data, segs[index], segs[index + 1], verbose)
+            data[output] = self._extract_values(raw_data, segs[index], segs[index + 1], verbose=verbose, category=output)
 
         return data
 
@@ -451,7 +478,7 @@ class Vicon(object):
 
         return fixed_names
 
-    def _extract_values(self, raw_data, start, end, verbose=False):
+    def _extract_values(self, raw_data, start, end, verbose=False, category=""):
         indices = {}
         data = {}
         current_name = None
@@ -483,6 +510,7 @@ class Vicon(object):
 
         # Put all the data in the correct sub dictionary.
 
+        flags = []
         for row in raw_data[start + 5:end - 1]:
 
             frame = int(row[0])
@@ -491,17 +519,30 @@ class Vicon(object):
                     index = indices[(key, sub_key)]
                     if row[index] is '':
                         val = np.nan
+                    elif '!' in row[index]:
+                        val = float(row[index][1:])
+                        if verbose and (key, sub_key) not in flags:
+                            print "Reading previously interpolated data in category " + category +\
+                                  ", subject " + key + ", field " + sub_key + "."
+                            flags.append((key, sub_key))
                     else:
                         val = float(row[index])
                     sub_value["data"].append(val)
         for key, value in data.iteritems():  # For every subject in the data...
             for sub_key, sub_value in value.iteritems():  # For each field under each subject...
-                #  If we have NaNs *and* the whole row isn't NaNs...
+                #  If we have NaNs and the whole row isn't NaNs...
                 #  No interpolation method can do anything with an array of NaNs,
                 #  so this way we save ourselves a bit of computation
-                if True in np.isnan(sub_value["data"]) and False in np.isnan(sub_value["data"]):
+                nans = np.isnan(sub_value["data"])
+                if True in nans and False in nans:
+                    if category not in self._nan_dict:
+                        self._nan_dict[category] = {}
+                    if key not in self._nan_dict[category]:
+                        self._nan_dict[category][key] = {}
+                    self._nan_dict[category][key][sub_key] = nans
                     if verbose:
-                        print "Interpolating missing values in field " + sub_key + ", in subject " + key + "..."
+                        print "Interpolating missing values in field " + sub_key + ", in subject " + key +\
+                              ", in category " + category + "..."
                     s = pandas.Series(sub_value["data"])
                     #  Akima interpolation only covers interior NaNs,
                     #  and splines are *way* too imprecise with unset boundary conditions,
@@ -509,8 +550,30 @@ class Vicon(object):
                     s = s.interpolate(method='akima', limit_direction='both')
                     s = s.interpolate(method='linear', limit_direction='both')
                     sub_value["data"] = s.to_list()
+                else:
+                    if category not in self._nan_dict:
+                        self._nan_dict[category] = {}
+                    if key not in self._nan_dict[category]:
+                        self._nan_dict[category][key] = {}
+                    self._nan_dict[category][key][sub_key] = self._false_of_n(len(sub_value["data"]))
 
         return data
+
+    def _false_of_n(self, n):
+        """Helper function to generate an array of Falses of length N"""
+        arr = []
+        for i in range(n):
+            arr.append(False)
+        return arr
+
+    def save(self, filename=None, verbose=False, mark_interpolated=True):
+        file_path = self._file_path
+        if filename is not None:
+            file_path = filename
+        if verbose and mark_interpolated:
+            print "Saving data to " + file_path + ". Interpolated values will be marked with '!'."
+        if verbose and not mark_interpolated:
+            print "Saving data to " + file_path + ". Interpolated values will not be marked."
 
 
 
