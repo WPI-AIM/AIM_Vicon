5e28478ee3168ed5f06d06a076f936d0cfa5f73e
nagoldfarb <nagoldfarb@wpi.edu>
2020-06-19 09:03:08 -0400

    #3 trying to set up packlages

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 import setuptools
+from setuptools import setup, find_packages
 
 setuptools.setup(
     name="Vicon",
     version="1.0",
     install_requires=["GaitCore @ git+https://github.com/WPI_AIM/AIM_GaitCore.git"],
-    packages=['Vicon', 'Devices', 'Markers']
+    packages=find_packages()
     #py_modules=["Vicon", "ModelOutput", "Markers", "IMU", "ForcePlate", "EMG", "Devices", "Accel"]
 )
+git
