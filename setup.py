d4964e1ea990bf3f7ea81ce934f83b8a1f8d2667
nagoldfarb <nagoldfarb@wpi.edu>
2020-06-19 11:38:39 -0400

    #3 get parent dir to work

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
-from setuptools import setup, find_packages
+from setuptools import  find_packages
 
 setuptools.setup(
     name="Vicon",
     version="1.0",
     install_requires=["GaitCore @ git+https://github.com/WPI-AIM/AIM_GaitCore.git"],
-    namespace_packages=['Vicon'],
+    package_dir={'': '.'},
     packages=find_packages()
     #py_modules=["Vicon", "ModelOutput", "Markers", "IMU", "ForcePlate", "EMG", "Devices", "Accel"]
 )
