86ef6d104b30cff4276e89d3f26fc27be14e4f98
nagoldfarb <nagoldfarb@wpi.edu>
2020-06-19 09:54:25 -0400

    #3 fixing gait core ref

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setuptools.setup(
     name="Vicon",
     version="1.0",
-    install_requires=["GaitCore @ git+https://github.com/WPI_AIM/AIM_GaitCore.git"],
+    install_requires=["GaitCore @ git+https://github.com/WPI-AIM/AIM_GaitCore.git"],
     packages=find_packages()
     #py_modules=["Vicon", "ModelOutput", "Markers", "IMU", "ForcePlate", "EMG", "Devices", "Accel"]
 )
