0f9173a95664997b826c87f507dc294a975efc8d
nagoldfarb <nagoldfarb@wpi.edu>
2020-08-05 07:56:27 -0400

    reseting setup pulls

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import  find_packages
 setuptools.setup(
     name="Vicon",
     version="1.0",
-    install_requires=["GaitCore @ git+https://github.com/nag92/AIM_GaitCore.git",
+    install_requires=["GaitCore @ git+https://github.com/WPI-AIM/AIM_GaitCore.git",
         "numpy",
         "scipy",
         "pandas"
