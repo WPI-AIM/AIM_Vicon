d83bdf2edb054aca7dfb0bc1b3922fe66f91edec
Alex Tacescu <alextac98@gmail.com>
2021-02-10 23:19:34 -0500

    Add SCoRE/SARA animation in AnimateModel class

--- Vicon/Tools/AnimateModel.py
+++ Vicon/Tools/AnimateModel.py
@@ -5,6 +5,8 @@ import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D as plot3d
 
 from GaitCore.Core.PointArray import PointArray
+from GaitCore.Bio.Sara import Sara
+from GaitCore.Bio.Score import Score
 
 class AnimateModel():
     def __init__(self,  window_title: str = "3D Marker Animation",
@@ -18,6 +20,10 @@ class AnimateModel():
         self._length = 0
         
         self.markers = {}
+
+        self.sara_dict = {}
+        self.score_dict = {}
+
         self.units = ""
 
         self.fig = plot.figure()
@@ -25,9 +31,9 @@ class AnimateModel():
 
         self.window_title = window_title
         self.chart_title = chart_title
-        self.x_label = x_label + " (in " + self.units + ")"
-        self.y_label = y_label + " (in " + self.units + ")"
-        self.z_label = z_label + " (in " + self.units + ")"
+        self.x_label = x_label
+        self.y_label = y_label
+        self.z_label = z_label
 
         self.ax = self.fig.add_subplot(111, projection='3d')
         self.ax.set_xlabel(x_label + " (in " + self.units + ")")
@@ -77,7 +83,15 @@ class AnimateModel():
         else:
             raise Exception("Error in AnimateModel: Marker sizes are not all the same")
 
-    def draw(self, interval: int = 2, save_animation: bool = False):
+    def import_joints(self):
+        print()
+
+    def import_sara(self, sara_data: dict):
+        self.sara_dict = sara_data
+    def import_score(self, score_data: dict):
+        self.score_dict = score_data
+
+    def draw(self, interval: int = 1, save_animation: bool = False):
 
         ani = animation.FuncAnimation(  fig = self.fig, 
                                         func = self._animate, 
@@ -97,9 +111,9 @@ class AnimateModel():
 
         # --- Clear and Set Up Figure ---- #
         self.ax.clear()
-        self.ax.set_xlabel(self.x_label)
-        self.ax.set_ylabel(self.y_label)
-        self.ax.set_zlabel(self.z_label)
+        self.ax.set_xlabel(self.x_label + " (in " + self.units + ")")
+        self.ax.set_ylabel(self.y_label + " (in " + self.units + ")")
+        self.ax.set_zlabel(self.z_label + " (in " + self.units + ")")
         self.ax.set_title = self.chart_title
 
         if self.x_limit is not None:
@@ -110,6 +124,7 @@ class AnimateModel():
             self.ax.set_zlim3d(list(self.z_limit))
 
         # --- Animate Markers --- #
+        marker_key = 'Markers'
         marker_poses_x = []
         marker_poses_y = []
         marker_poses_z = []
@@ -118,7 +133,28 @@ class AnimateModel():
             marker_poses_y.append(value.get(i).y)
             marker_poses_z.append(value.get(i).z)
         
-        self.ax.scatter(marker_poses_x,
-                        marker_poses_y,
-                        marker_poses_z,
-                        c = 'r', marker = 'o')
+        marker_ax = self.ax.scatter(marker_poses_x,
+                                    marker_poses_y,
+                                    marker_poses_z,
+                                    label = 'Markers',
+                                    c = 'r', marker = 'o')
+        
+        # --- Animate Joints --- #
+
+
+        # --- Animate Score/Sara --- #
+        for key, sara in self.sara_dict.items():
+            self.ax.scatter(sara.x_array[i],
+                            sara.y_array[i],
+                            sara.z_array[i],
+                            label = key,
+                            c = 'm', marker = 'o')
+
+        for key, score in self.score_dict.items():
+            self.ax.scatter(score.x_array[i],
+                            score.y_array[i],
+                            score.z_array[i],
+                            label = key,
+                            c = 'g', marker = 'o')
+
+        plot.legend(loc='upper right', ncol=1, fontsize=8)
